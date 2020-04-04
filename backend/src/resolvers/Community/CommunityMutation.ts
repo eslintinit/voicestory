1c0530adcb660dc265887b922a6d9cf3d26d0af8
alokin1990 <nefremovski@gmail.com>
2020-04-04 17:57:03 +0200

    add delteCommunityByUrl

--- backend/src/resolvers/Community/CommunityMutation.ts
+++ backend/src/resolvers/Community/CommunityMutation.ts
@@ -54,12 +54,25 @@ export const unfollowCommunity = mutationField('unfollowCommunity', {
 export const deleteCommunity = mutationField('deleteCommunity', {
   type: 'Community',
   args: {
-    channelId: stringArg(),
+    communityId: stringArg(),
   },
-  resolve: async (parent, { channelId }, ctx) => {
+  resolve: async (parent, { communityId }, ctx) => {
     const userId = getUserId(ctx)
     return ctx.prisma.community.delete({
-      where: { id: channelId },
+      where: { id: communityId },
+    })
+  },
+})
+
+export const deleteCommunityByUrl = mutationField('deleteCommunityByUrl', {
+  type: 'Community',
+  args: {
+    communityUrl: stringArg(),
+  },
+  resolve: async (parent, { communityUrl }, ctx) => {
+    const userId = getUserId(ctx)
+    return ctx.prisma.community.delete({
+      where: { url: communityUrl },
     })
   },
 })
