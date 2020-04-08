e4e18914c0912109261d450379c8a42ab4f95115
alokin1990 <nefremovski@gmail.com>
2020-04-08 20:09:17 +0200

    Add crud & refactor User Queries

--- backend/src/resolvers/Query.ts
+++ backend/src/resolvers/Query.ts
@@ -1,17 +1,33 @@
 import * as UserQuery from './User/UserQuery'
 import * as CommunityQuery from './Community/CommunityQuery'
+import { queryField, queryType, stringArg } from 'nexus'
+
 import * as ChannelQuery from './Channel/ChannelQuery'
 import * as MessageQuery from './Message/MessageQuery'
 import * as FileQuery from './File/FileQuery'
 import * as RoleQuery from './Role/RoleQuery'
+import { getUserId, isEmpty } from '../utils'
+import { isContext } from 'vm'
+
+export const queries = queryType({
+  definition(t) {
+    // crud for USERS
+    t.crud.users({
+      alias: 'users',
+      filtering: true,
+      ordering: true,
+    })
+  },
+})
 
 export const Query = {
-  me: { UserQuery },
-  getUser: { UserQuery },
-  users: { UserQuery },
-  community: { CommunityQuery },
-  communities: { CommunityQuery },
-  searchCommunities: { CommunityQuery },
+  queries,
+  UserQuery,
+
+  // // users: { UserQuery },
+  // // community: { CommunityQuery },
+  // // communities: { CommunityQuery },
+  // searchCommunities: { CommunityQuery },
   /* channel: { ChannelQuery }, */
   /* channels: { ChannelQuery }, */
   /* privateChannels: { ChannelQuery }, */
