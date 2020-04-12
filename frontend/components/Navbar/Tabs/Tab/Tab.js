139fd1cbbcfe67a21f0ef424250f997932205258
alokin1990 <nefremovski@gmail.com>
2020-04-12 23:06:17 +0200

    Add unffollow on X navbar

--- frontend/components/Navbar/Tabs/Tab/Tab.js
+++ frontend/components/Navbar/Tabs/Tab/Tab.js
@@ -1,10 +1,13 @@
 import { useRouter } from 'next/router'
 import { useKeyboardShortcut } from 'hooks'
+import { useMutation } from '@apollo/react-hooks'
 
 import { COMPANY_NAME } from 'utils/config'
 
 import closeSVG from 'public/icons/close.svg'
 import * as S from './Tab.styled'
+
+import { UNFOLLOW_COMMUNITY, CommunityFragment } from 'apis/Community'
 
 const Tab = ({ community, active, nextActive, index }) => {
   const router = useRouter()
@@ -23,6 +26,19 @@ const Tab = ({ community, active, nextActive, index }) => {
     [index + 1]: onChangeTab,
   })
 
+  const [unfollowCommunity] = useMutation(UNFOLLOW_COMMUNITY, {
+    update(cache, { data: { unfollowCommunity: community } }) {
+      const fragment = CommunityFragment
+      const cachedCommunity = cache.readFragment({
+        fragment,
+        __typename: 'Community',
+      })
+      const updatedCommunity = { ...cachedCommunity }
+
+      return cache.writeFragment({ fragment, updatedCommunity })
+    },
+  })
+
   return (
     <S.Tab active={active} onClick={onChangeTab} key={community.id}>
       <S.TabContent active={active} nextActive={nextActive}>
@@ -34,7 +50,7 @@ const Tab = ({ community, active, nextActive, index }) => {
           <S.UnsubscribeIcon
             onClick={() => {
               // Do: Unfollow. Update cache
-              // unfollowCommunity({ variables: { url: community.url } });
+              unfollowCommunity({ variables: { url: community.url } })
             }}
           >
             <use xlinkHref={`${closeSVG}#icon-close`} />
