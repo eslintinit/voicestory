cab97f7e96ecf920bf0fdeabe9a48b89c138d97b
alokin1990 <nefremovski@gmail.com>
2020-04-13 16:56:36 +0200

    filter followed if user has followed any

--- frontend/components/Navbar/Tabs/Tabs.js
+++ frontend/components/Navbar/Tabs/Tabs.js
@@ -2,7 +2,7 @@ import { useRouter } from 'next/router'
 import { useQuery, writeQuery } from '@apollo/react-hooks'
 import { useContext, useState, useEffect } from 'react'
 
-import { GET_COMMUNITIES_CLIENT, GET_COMMUNITIES } from 'apis/Community'
+import { GET_COMMUNITIES } from 'apis/Community'
 
 import Tab from './Tab/Tab'
 import * as S from './Tabs.styled'
@@ -16,28 +16,29 @@ const Tabs = () => {
   const { loading, data, error } = useQuery(GET_COMMUNITIES)
   return data && data.communities.length > 0 ? (
     <S.Tabs>
-      {data.communities
-        .filter((a) => a.isFollowed === true)
-        .map((community, index) => {
-          const active = community.url === selectedCommunity
+      {(data.communities.filter((a) => a.isFollowed === true).length === 0
+        ? data.communities
+        : data.communities.filter((a) => a.isFollowed === true)
+      ).map((community, index) => {
+        const active = community.url === selectedCommunity
 
-          // Done for styles
-          const nextActive =
-            index < communities.length - 1
-              ? communities[index + 1].url === selectedCommunity
-              : false
+        // Done for styles
+        const nextActive =
+          index < communities.length - 1
+            ? communities[index + 1].url === selectedCommunity
+            : false
 
-          return (
-            <Tab
-              community={community}
-              active={active}
-              index={index}
-              key={community.id}
-              nextActive={nextActive}
-              numberOfCommunities={communities.length}
-            />
-          )
-        })}
+        return (
+          <Tab
+            community={community}
+            active={active}
+            index={index}
+            key={community.id}
+            nextActive={nextActive}
+            numberOfCommunities={communities.length}
+          />
+        )
+      })}
     </S.Tabs>
   ) : (
     ''
